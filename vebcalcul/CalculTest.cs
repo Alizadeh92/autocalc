28506fcb586bdc7aba48a67dc3d2d78569f8864a
EeSmile <ta.ismail@mail.ru>
2020-06-18 21:56:58 +0400

    Добавьте файлы проекта.

---
+++ vebcalcul/CalculTest.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using vebcalcul;
+
+namespace Webcalc
+{
+    [TestFixture]
+    class CalculTest
+    {
+        Program calc = new Program();
+        IWebDriver drive;
+
+        [TestCase("5", "3", "8")]
+        [TestCase("2", "2", "4")]
+        public void TestPlus(string num1, string num2, string exp)
+        {
+            drive = new ChromeDriver();
+            drive.Navigate().GoToUrl("file:///C:/Users/%D0%90%D0%B4%D0%BC%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%BE%D1%80/Desktop/HW14-master/refactored/index.html");
+
+            drive.FindElement(By.Id(num1)).Click();
+            drive.FindElement(By.Id("+")).Click();
+            drive.FindElement(By.Id(num2)).Click();
+            drive.FindElement(By.Id("equal")).Click();
+            IWebElement output = drive.FindElement(By.Id("15"));
+            string act = output.GetAttribute("value");
+            Assert.AreEqual(exp, act);
+            drive.Quit();
+        }
+        [TestCase("7", "2", "5")]
+        [TestCase("9", "4", "5")]
+        public void TestMinus(string num1, string num2, string exp)
+        {
+            drive = new ChromeDriver();
+            drive.Navigate().GoToUrl("file:///C:/Users/%D0%90%D0%B4%D0%BC%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%BE%D1%80/Desktop/HW14-master/refactored/index.html");
+
+            drive.FindElement(By.Id(num1)).Click();
+
+            drive.FindElement(By.Id("-")).Click();
+            drive.FindElement(By.Id(num2)).Click();
+            drive.FindElement(By.Id("equal")).Click();
+            IWebElement output = drive.FindElement(By.Id("15"));
+            string act = output.GetAttribute("value");
+            Assert.AreEqual(exp, act);
+
+        }
+        [TestCase("2", "2", "4")]
+        [TestCase("9", "5", "45")]
+        public void TestUmn(string num1, string num2, string exp)
+        {
+            drive = new ChromeDriver();
+            drive.Navigate().GoToUrl("file:///C:/Users/%D0%90%D0%B4%D0%BC%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%BE%D1%80/Desktop/HW14-master/refactored/index.html");
+
+            drive.FindElement(By.Id(num1)).Click();
+
+            drive.FindElement(By.Id("*")).Click();
+            drive.FindElement(By.Id(num2)).Click();
+            drive.FindElement(By.Id("equal")).Click();
+            IWebElement output = drive.FindElement(By.Id("15"));
+            string act = output.GetAttribute("value");
+            Assert.AreEqual(exp, act);
+
+        }
+        [TestCase("7", "7", "1")]
+        [TestCase("6", "3", "2")]
+        public void TestDel(string num1, string num2, string exp)
+        {
+            drive = new ChromeDriver();
+            drive.Navigate().GoToUrl("file:///C:/Users/%D0%90%D0%B4%D0%BC%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%BE%D1%80/Desktop/HW14-master/refactored/index.html");
+            drive.FindElement(By.Id(num1)).Click();
+            drive.FindElement(By.Id("/")).Click();
+            drive.FindElement(By.Id(num2)).Click();
+            drive.FindElement(By.Id("equal")).Click();
+            IWebElement output = drive.FindElement(By.Id("15"));
+            string act = output.GetAttribute("value");
+            Assert.AreEqual(exp, act);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            drive.Quit();
+        }
+    }
+}
---
+++ vebcalcul/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("vebcalcul")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("vebcalcul")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4beda845-a86b-4485-8a1b-2c44a8956300")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
